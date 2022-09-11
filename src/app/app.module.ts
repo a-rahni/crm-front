f85061001f69559f0c9d793ff5eb43c21b7ac221
a rahni <rahniahmed@gmail.com>
2022-09-11 16:30:38 +0200

    squelette ui chat responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Grid12Component } from './grid12/grid12.component';
+import { UiChatComponent } from './ui-chat/ui-chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Grid12Component
+    Grid12Component,
+    UiChatComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ui-chat/ui-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-chat',
+  templateUrl: './ui-chat.component.html',
+  styleUrls: ['./ui-chat.component.scss']
+})
+export class UiChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
